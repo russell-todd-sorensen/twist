23fba904f5581fe189a31ad9972a956a234ed73a
Russell Sorensen <russell-github-commits@highfivediet.com>
2021-10-18 12:58:36 -0700

    Updated stockQuoter example with new name!type separator

--- packages/stockQuoter/tcl/stockQuoter-procs.tcl
+++ packages/stockQuoter/tcl/stockQuoter-procs.tcl
@@ -40,34 +40,33 @@ set sqns "stockquoter"
 ::wsdl::types::simpleType::restrictByEnumeration $sqns faultCode xsd::integer {404 500 301}
 # Note that the command returns a script. eval executes it.
 eval [::wsdl::elements::modelGroup::sequence::new $sqns StockRequest {
-    {Symbol:stockquoter::symbol}
-    {Verbose:stockquoter::verbose {minOccurs 0 default "1"}}
+    {Symbol!stockquoter::symbol}
+    {Verbose!stockquoter::verbose {minOccurs 0 default "1"}}
 }]
-	  
 
 
 eval [::wsdl::elements::modelGroup::sequence::new $sqns StockQuote {
-    {Symbol:stockquoter::symbol          }
-    {Quote:stockquoter::quote           }
-    {DateOfChange:stockquoter::dateOfChange {minOccurs 0}  }
-    {Name:stockquoter::name         {minOccurs 0 nillable true}}
-    {Trend:stockquoter::trend        {minOccurs 0}}
-    {DailyMove:stockquoter::dailyMove    {minOccurs 0}  }
-    {LastMove:stockquoter::lastMove     {minOccurs 0}  }
+    {Symbol!stockquoter::symbol          }
+    {Quote!stockquoter::quote           }
+    {DateOfChange!stockquoter::dateOfChange {minOccurs 0}  }
+    {Name!stockquoter::name         {minOccurs 0 nillable true}}
+    {Trend!stockquoter::trend        {minOccurs 0}}
+    {DailyMove!stockquoter::dailyMove    {minOccurs 0}  }
+    {LastMove!stockquoter::lastMove     {minOccurs 0}  }
 }]
 
 eval [::wsdl::elements::modelGroup::sequence::new $sqns StockQuoteFault {
-    {FaultCode:stockquoter::faultCode        }
+    {FaultCode!stockquoter::faultCode        }
 }]
 
 eval [::wsdl::elements::modelGroup::sequence::new $sqns StockRequests {
-    {Symbol:stockquoter::symbol {maxOccurs 8 default "MSFT"}}
-    {Verbose:stockquoter::verbose {minOccurs 0 default "1"}}
+    {Symbol!stockquoter::symbol {maxOccurs 8 default "MSFT"}}
+    {Verbose!stockquoter::verbose {minOccurs 0 default "1"}}
 }]
 
 
 eval [::wsdl::elements::modelGroup::sequence::new $sqns StockQuotes {
-    {StockQuote:elements::stockquoter::StockQuote {maxOccurs 8}}
+    {StockQuote!elements::stockquoter::StockQuote {maxOccurs 8}}
 }]
 
 
@@ -80,8 +79,8 @@ eval [::wsdl::messages::new $sqns StockQuotes StockQuotes]
 
 # tmp operations:
 eval [::wsdl::operations::new $sqns StockQuoteOperation {::sq::mystockquote {
-    {Symbol:element::stockquoter::Symbol}
-    {Verbose:element::stockquoter::Verbose {minOccurs 0 default "0"}}
+    {Symbol!element::stockquoter::Symbol}
+    {Verbose!element::stockquoter::Verbose {minOccurs 0 default "0"}}
 }}  {input StockRequest} {output StockQuote} {fault StockQuoteFault}]
 
 eval [::wsdl::operations::new stockquoter StockQuotesOperation {
@@ -109,9 +108,9 @@ proc ::sq::mystockquote { symbol {verbose 1} } {
 
     set StockValue [format %0.2f [expr 25.00 + [ns_rand 4].[format %0.2d [ns_rand 99]]]]
     if {$verbose} {
-	return [list $symbol $StockValue 2006-04-11T00:00:00Z "Microsoft Corp." 1 0.75 0.10]
+        return [list $symbol $StockValue 2006-04-11T00:00:00Z "Microsoft Corp." 1 0.75 0.10]
     } else {
-	return [list $symbol $StockValue]
+        return [list $symbol $StockValue]
     }
 }
 
@@ -120,7 +119,7 @@ proc ::sq::StockQuotes {SymbolList Verbose} {
 
     set resultList [list]
     foreach symbol $SymbolList {
-	lappend resultList [::sq::mystockquote $symbol $Verbose]
+        lappend resultList [::sq::mystockquote $symbol $Verbose]
     }
     return $resultList
 }
